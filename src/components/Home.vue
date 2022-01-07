66dcd630c8db1d7f924bd4e3f51f42ec36744049
Bao Phung Gia <bao.phung@onemount.com>
2022-01-07 07:12:00 +0700

    add layout

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="homeContainer">
+    <a-layout class="homeLayout">
+      <a-layout-header class="homeHeader">Header</a-layout-header>
+      <a-layout-content class="homeContent">
+        <ProductCategory />
+      </a-layout-content>
+      <a-layout-footer class="homeFooter">Footer</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+<script>
+import ProductCategory from './ProductCategory/ProductCategory.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    ProductCategory
+  }
+}
+</script>
---
+++ src/components/ProductCategory/ProductCategory.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="productCategoryContainer">
+    <a-row class="productCategoryRow" type="flex" justify="space-around">
+      <a-col :xs="{ span: 6, offet: 1}" :sm="{ span: 6}" :md="{ span: 6}" :lg="{ span: 3}" :xl="{ span: 3}">
+        <a-card hoverable>
+          <img
+            slot="cover"
+            alt="example"
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
+          <a-card-meta title="Europe Street beat">
+            <template slot="description"> www.instagram.com </template>
+          </a-card-meta>
+        </a-card>
+      </a-col>
+      <a-col :xs="{ span: 6}" :sm="{ span: 6}" :md="{ span: 6}" :lg="{ span: 3}" :xl="{ span: 3}">
+        <a-card hoverable>
+          <img
+            slot="cover"
+            alt="example"
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
+          <a-card-meta title="Europe Street beat">
+            <template slot="description"> www.instagram.com </template>
+          </a-card-meta>
+        </a-card>
+      </a-col>
+      <a-col :xs="{ span: 6}" :sm="{ span: 6}" :md="{ span: 6}" :lg="{ span: 3}" :xl="{ span: 3}">
+        <a-card hoverable>
+          <img
+            slot="cover"
+            alt="example"
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
+          <a-card-meta title="Europe Street beat">
+            <template slot="description"> www.instagram.com </template>
+          </a-card-meta>
+        </a-card>
+      </a-col>
+      <a-col :xs="{ span: 6}" :sm="{ span: 6}" :md="{ span: 6}" :lg="{ span: 3}" :xl="{ span: 3}">
+        <a-card hoverable>
+          <img
+            slot="cover"
+            alt="example"
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
+          <a-card-meta title="Europe Street beat">
+            <template slot="description"> www.instagram.com </template>
+          </a-card-meta>
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+<script>
+export default {
+  name: "ProductCategory",
+};
+</script>
+<style scoped>
+</style>

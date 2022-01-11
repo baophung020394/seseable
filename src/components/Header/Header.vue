326a343501769c6f3b4791e011266532fa9281f8
Gia Bao <bao.phung@onemount.com>
2022-01-11 18:02:45 +0700

    fix

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -3,8 +3,36 @@
     <div class="headerLogo">
       <a href="/">Seseable</a>
     </div>
-    <div class="headerNav"></div>
-    <div class="headerOptions"></div>
+    <div class="headerNav">
+      <ul class="headerMainMenu">
+        <li>
+          <a href="#">Menu 1</a>
+          <ul class="headerSubMenu">
+            <li>
+              <a href="#">Menu 2</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Menu 2</a>
+        </li>
+        <li>
+          <a href="#">Menu 3</a>
+        </li>
+        <li>
+          <a href="#">Menu 4</a>
+        </li>
+        <li>
+          <a href="#">Menu 5</a>
+        </li>
+        <li>
+          <a href="#">Menu 6</a>
+        </li>
+      </ul>
+    </div>
+    <div class="headerOptions">
+      Options
+    </div>
   </div>
 </template>
 
@@ -15,10 +43,13 @@ export default {
 </script>
 
 <style lang="scss">
-.headerLogo {
-  a {
-    display: inline-block;
-    text-transform: uppercase;
+.headerContainer {
+  .headerLogo {
+    a {
+      display: inline-block;
+      text-transform: uppercase;
+    }
   }
+  .headerNav
 }
 </style>

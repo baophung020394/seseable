f27657b617fb7c996b9e878e0716b6ef7bdb936c
Gia Bao <bao.phung@onemount.com>
2022-01-14 17:09:04 +0700

    fixed

--- src/components/PageSidebar/PageSidebar.vue
+++ src/components/PageSidebar/PageSidebar.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="pageSidebarContainer">
+  <div class="pageSidebarContainer wrapper">
     <a-layout class="pageSidebarLayout">
       <a-layout-sider class="pageSidebarSider">Sider</a-layout-sider>
       <a-layout-content class="pageSidebarContent">
         <a-list
           class="demo-loadmore-list"
           :loading="loading"
-          item-layout="horizontal"
+          item-layout="vertical"
           :data-source="data"
         >
           <div
@@ -17,19 +17,21 @@
             <a-spin v-if="loadingMore" />
             <a-button v-else @click="onLoadMore"> loading more </a-button>
           </div>
-          <a-list-item slot="renderItem" slot-scope="item">
-            <a slot="actions">edit</a>
-            <a slot="actions">more</a>
-            <a-list-item-meta
-              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-            >
-              <a slot="title" href="https://www.antdv.com/">{{ item.name.last }}</a>
-              <a-avatar
-                slot="avatar"
-                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-              />
-            </a-list-item-meta>
-            <div>content</div>
+          <a-list-item slot="renderItem" slot-scope="item" class="pageSidebarCards">
+            <a-card hoverable class="pageSidebarCard">
+              <a href="#" class="pageSidebarLink">
+                <img
+                  slot="cover"
+                  alt="example"
+                  :src="item.image"
+                />
+              </a>
+              <a href="#" class="pageSidebarTitleLink">
+                <a-card-meta class="pageSidebarTitle" :title="item.title">
+                </a-card-meta>
+              </a>
+              <span class="pageSidebarPrice">{{item.price}}</span>
+            </a-card>
           </a-list-item>
         </a-list>
       </a-layout-content>
@@ -38,10 +40,10 @@
 </template>
 
 <script>
-import reqwest from 'reqwest';
+// import reqwest from 'reqwest';
+// const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
 import mockData from '../../mockData/mockData.json';
 
-// const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
 export default {
   name: 'PageSidebar',
   data() {
@@ -53,40 +55,87 @@ export default {
     };
   },
   mounted() {
-    this.getData((res) => {
-      this.loading = false;
-      this.data = res;
-    });
+    this.data = mockData?.results;
+    this.loading = false;
+    // this.getData((res) => {
+    //   this.loading = false;
+    //   this.data = res?.results;
+    // });
   },
   methods: {
-    getData(callback) {
-      reqwest({
-        url: mockData,
-        type: 'json',
-        method: 'get',
-        contentType: 'application/json',
-        success: (res) => {
-          callback(res);
-        },
-      });
-    },
+    // getData(callback) {
+    //   reqwest({
+    //     url: fakeDataUrl,
+    //     type: 'json',
+    //     method: 'get',
+    //     contentType: 'application/json',
+    //     success: (res) => {
+    //       callback(res);
+    //     },
+    //   });
+    // },
     onLoadMore() {
       this.loadingMore = true;
-      this.getData((res) => {
-        this.data = res;
+      setTimeout(() => {
+        this.data = this.data.concat(mockData.results)
         this.loadingMore = false;
-        this.$nextTick(() => {
-          window.dispatchEvent(new Event('resize'));
-        });
-      });
+      }, 1000);
+      
+      // this.getData((res) => {
+      //   this.data = this.data.concat(res.results);
+      //   this.loadingMore = false;
+      //   this.$nextTick(() => {
+      //     window.dispatchEvent(new Event('resize'));
+      //   });
+      // });
     },
   },
 };
-console.log('mockData', mockData);
+
 </script>
 
 <style lang="scss">
 .demo-loadmore-list {
   min-height: 350px;
+}
+.pageSidebarContainer {
+  background: #ffffff !important;
+  .pageSidebarSider,
+  .pageSidebarLayout {
+    background: #ffffff !important;
+  }
+  .pageSidebarSider {
+    display: none;
+  }
+  .pageSidebarContent {
+    .ant-list-items {
+      display: flex;
+      flex-direction: column;
+      padding: 0 30px;
+      .pageSidebarCards {
+        border-bottom: none;
+        .pageSidebarCard {
+            max-width: 236px;
+            img {
+              width: 100%;
+            }
+          }
+      }
+    }
+  }
+}
+@media screen and (min-width:1200px) {
+  .pageSidebarContainer {
+    .pageSidebarSider {
+      display: inline-block;
+    }
+    .pageSidebarContent {
+      .ant-list-items {
+        flex-wrap: wrap;
+        flex-direction: row;
+        justify-content: space-between;
+      }
+    }
+  }
 }
 </style>
